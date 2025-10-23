cf08f98128b6ff7b1ca17bc7a754f7d3aef90c22
So-dl <clarence_so@dlsu.edu.ph>
2025-10-24 03:05:07 +0800

    Update BlueDoor.java

--- BlueDoor.java
+++ BlueDoor.java
@@ -1,6 +1,5 @@
-public class BlueDoor extends Doors{
-    	BlueDoor(String doorName){
-        super(doorName, false);
-		this.unlocked = false;
+public class BlueDoor extends Tiles{
+    	BlueDoor(){
+        super("BlueDoor", false);
     }
 }
