d344f9cf79cca0807e64248d3fca47be7a7a9085
So-dl <clarence_so@dlsu.edu.ph>
2025-10-24 00:06:23 +0800

    Update NormalTile.java

--- NormalTile.java
+++ NormalTile.java
@@ -1,8 +1,10 @@
 public class NormalTile extends Tiles {
 
     private boolean hasItem;
+    private Item item;
     NormalTile(String tileName, boolean hasItem){
         super(tileName, true, false);
         this.hasItem = hasItem;
     }
+
 }
